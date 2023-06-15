3b38f8e8dc913aff280bfd141f61d523d9170ac6
SekharGenala <93277871+SekharGenala@users.noreply.github.com>
2023-06-15 12:12:43 +0530

    Update combined.py

--- combined.py
+++ combined.py
@@ -3,7 +3,7 @@ import os
 import pdf2docx
 from werkzeug.utils import secure_filename
 from docx2pdf import convert
-import comtypes
+import comtypes.client
 import openpyxl
 from PIL import Image
 from fpdf import FPDF
